7eacaac776d9794d9943e3ce929d384ec1c4fcae
Ignacio Sotelo <ignaciosotelo8@gmail.com>
2022-09-05 17:39:58 -0300

    Part1

--- part1-react/courseinfo/src/index.js
+++ part1-react/courseinfo/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Header = (props) => {
-  console.log(props)
-    return <h1>{props.course}</h1>
+const Header = ({course}) => {
+  console.log(course)
+    return <h1>{course}</h1>
 }
 
 const Part = (props) => {
